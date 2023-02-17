ae69c2e93ead44815d6d5e5606cb4cd0d955ad0f
Abel <belaashu23@gmail.com>
2023-02-17 12:27:58 +0300

    Alterd the blink duration with pushbuttons

--- main.c
+++ main.c
@@ -12,22 +12,70 @@
 #pragma config WDT = OFF			// Disable Watch Dog Timer 
 #pragma config OSC = HS				// Use an External Crystal Oscillator 
 
+void Blinking (unsigned char);
+void DebounceDelay (void); 
+
+void Blinking (unsigned char Speed)
+{
+	LATCbits.LATC0 	 = 1;		// Turn on LED for a while 
+	Delay100TCYx(Speed * 100);
+	
+	LATCbits.LATC0 	 = 0;		// Turn on LED for a while 
+	Delay100TCYx(Speed * 100);
+} 
+
+void DebounceDelay (void)
+{
+	Delay10KTCYx (50); 
+} 
+
 void main (void)
 {
+	unsigned char Count = 1; 
+	
 	TRISCbits.TRISC0 = 0;			// Make PORTC0 an output port 
 	LATCbits.LATC0 	 = 0; 			// Initialize PORTC0 to zero 
 	
+	TRISDbits.TRISD0 = 1; 			// Make PortD0 an input port 
+	
 	while (1)						// An infinite loop 
 	{
-		LATCbits.LATC0 	 = 1;		// Turn on LED for a while 
- 		Delay10KTCYx(100);			// A Delay of 1 seconds @ 4MHz
- 		
- 		/* assuming that we are using a clock frequency of 4MHz, 
- 		   1TCY = 1 / (4MHz /4), in this case 1TCY = 1us (one microseconds)
- 		*/
- 		
-		LATCbits.LATC0 	 = 0;		// Turn off LED for a while
-		Delay10KTCYx(100);			// A Delay of 1 seconds @ 4MHz
+		if (PORTDbits.RD0 == 1)
+		{
+			DebounceDelay (); 
+			
+			Count += 1; 
+			
+			if (Count > 5)
+				Count = 1; 
+		}
+		
+		switch (Count)
+		{
+			case 1: 
+				Blinking (1); 
+			break;
+			
+			case 2: 
+				Blinking (2); 
+			break;
+			
+			case 3: 
+				Blinking (3); 
+			break;
+			
+			case 4: 
+				Blinking (4); 
+			break;
+			
+			case 5: 
+				Blinking (5); 
+			break; 
+			
+			default: 
+				Blinking (1); 
+			break; 
+		}
 	}
 }
 
